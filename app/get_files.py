79e6bbb84b61f91d6c891bbb2afed1c46e8281b8
ben05allen <ben05allen@hotmail.com>
2024-10-29 09:15:15 +0900

    structuring main

--- app/get_files.py
+++ app/get_files.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 
 
-def find_files_with_extensions(folder_path, extensions):
+def find_files_with_extensions(folder_path, extensions, is_recursive: bool = True):
     extensions = set(ext.lower() for ext in extensions)
 
     # Recursively search for files with the specified extensions
-    for file in Path(folder_path).rglob("*"):
-        if file.suffix.lower() in extensions:
-            yield file.resolve()
+    if is_recursive:
+        for file in Path(folder_path).rglob("*"):
+            if file.suffix.lower() in extensions:
+                yield file.resolve()
+    else:
+        for file in Path(folder_path).iterdir():
+            if file.is_file() and file.suffix.lower() in extensions:
+                yield file.resolve()
+    
