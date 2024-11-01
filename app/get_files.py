cc623b36779af9cf6b369fbe41e804f51c6d0aaa
ben05allen <ben05allen@hotmail.com>
2024-11-02 08:53:39 +0900

    allow paths or strings in search

--- app/get_files.py
+++ app/get_files.py
@@ -1,16 +1,20 @@
 from pathlib import Path
+from typing import Generator, Iterable
 
 
-def find_files_with_extensions(folder_path, extensions, is_recursive: bool = True):
+def find_files_with_extensions(
+    folder_path: str | Path, extensions: Iterable[str], is_recursive: bool = True
+) -> Generator[Path, None, None]:
     extensions = set(ext.lower() for ext in extensions)
 
-    # Recursively search for files with the specified extensions
+    if isinstance(folder_path, str):
+        folder_path = Path(folder_path)
+
     if is_recursive:
-        for file in Path(folder_path).rglob("*"):
-            if file.suffix.lower() in extensions:
-                yield file.resolve()
+        folder_search = folder_path.rglob("*")
     else:
-        for file in Path(folder_path).iterdir():
-            if file.suffix.lower() in extensions:
-                yield file.resolve()
-    
+        folder_search = folder_path.iterdir()
+
+    for file in folder_search:
+        if file.suffix.lower() in extensions:
+            yield file.resolve()
